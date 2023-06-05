1aae34cd413658e36f4e821ff504389643c413a5
Boniel-M <bonielwanjohi@gmail.com>
2023-06-05 09:58:09 -0700

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
+str = "Python is an interpreted, interactive, object-oriented programming
+language that combines remarkable power with very clear syntax"
+print(str[:71] + "\n")
