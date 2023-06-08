a52ddc7045a2850e2c442002af7d6b31f888acf7
Boniel-M <bonielwanjohi@gmail.com>
2023-06-08 04:39:44 -0700

    The program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import add_0 as Boniel
+"""program that imports the function def add(a, b): from the file add_0.py"""
+a = 1
+b = 2
+
+sum = Boniel.add(a, b)
+
+print("{} + {} = {}".format(a, b, sum))
