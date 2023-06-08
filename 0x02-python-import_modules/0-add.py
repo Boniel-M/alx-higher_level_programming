91f5fdfa598fcaeb966e790c40e5a0e022f6f391
Boniel-M <bonielwanjohi@gmail.com>
2023-06-08 06:02:29 -0700

    The program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,14 +2,15 @@
 
 """program that imports the function def add(a, b): from the file add_0.py"""
 
+from add_0 import add
+
+a = 1
+b = 2
+
+sum = add(a, b)
+
+print("{} + {} = {}".format(a, b, sum))
+print("", end="")
+
 if __name__ == "__main__":
-    import sys
-    from add_0 import add
-
-    a = 1
-    b = 2
-
-    sum = add(a, b)
-
-    print("{} + {} = {}".format(a, b, sum))
-    print("", end="")
+    add(a, b)
