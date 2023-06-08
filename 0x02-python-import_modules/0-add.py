d2c12f21ae3853996c00447f299cb8b2c593e269
Boniel-M <bonielwanjohi@gmail.com>
2023-06-08 05:23:21 -0700

    The program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
 
 """program that imports the function def add(a, b): from the file add_0.py"""
-from add_0 import add as Boniel
-    a = 1
-    b = 2
 
-    sum = Boniel(a, b)
+import add_0 as add
 
-    print("{} + {} = {}".format(a, b, sum))
-    print("", end="")
+a = 1
+b = 2
+
+sum = add.add(a, b)
+
+print("{} + {} = {}".format(a, b, sum))
+print("", end="")
