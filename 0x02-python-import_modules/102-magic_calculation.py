849e5d5fdb843c05a999388a161958e501c126a6
Boniel-M <bonielwanjohi@gmail.com>
2023-06-08 12:27:25 -0700

    Python function def magic_calculation(a, b): thaat creates a python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,6 +2,8 @@
 
 def magic_calculation(a, b):
 
+    """function emulates the behavior of the given Python bytecode"""
+
     add, sub = __import__("magic_calculation_102", fromlist=("add", "sub"))
 
     if a < b:
