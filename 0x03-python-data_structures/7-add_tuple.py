a5551cc92cecca28469eb29b50543cd58b32d0e3
Boniel-M <bonielwanjohi@gmail.com>
2023-06-12 02:05:01 -0700

    Function that add 2 tupples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2] if len(tuple_a) >= 2 else (0, 0)
-    b1, b2 = tuple_b[:2] if len(tuple_b) >= 2 else (0, 0)
+    """# Extract the first two elements from tuple_a, or
+    use 0 if the tuple is smaller than 2"""
 
-    sum_1 = a1 + b1
-    sum_2 = a2 + b2
+    a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
 
-    return sum_1, sum_2
+    """ # Extract the first two elements from tuple_b,
+    #or use 0 if the tuple is smaller than 2"""
+    b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
+
+    """# Perform addition of corresponding elements
+    and return the resulting tuple"""
+    return a1 + b1, a2 + b2
