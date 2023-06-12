c84a5731451c2a558a078746ae44130da3da4d6c
Boniel-M <bonielwanjohi@gmail.com>
2023-06-12 02:22:25 -0700

    The function function prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, num in enumerate(row):
-            """# Print the number with a space separator"""
-            print("{:d}".format(num), end=" ")
-
-            """# Print a newline character after the last number in the row"""
-            if i == len(row) - 1:
-                print()
-
-    """# Print an empty line if the matrix is empty"""
-    if not matrix:
-        print()
+            if i != len(row) - 1:
+                print("{:d} ".format(num), end='')
+            else:
+                print("{:d}".format(num))
