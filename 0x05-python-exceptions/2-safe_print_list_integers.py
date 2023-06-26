1cd41dc16b01ff674931301b9a2b04fbd5bc2ac5
Boniel-M <bonielwanjohi@gmail.com>
2023-06-26 04:21:12 -0700

    added a raise function to display error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,14 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
+    try:
+        for i in range(x):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-        except IndexError:
-            break
-    print()
+    except IndexError:
+        print()
+        raise
+    else:
+        print()
     return count
