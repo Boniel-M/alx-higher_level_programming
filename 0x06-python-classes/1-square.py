58310025a446fab9cdfe7c4f7cdfc79868b1e4ae
Boniel-M <bonielwanjohi@gmail.com>
2023-06-27 02:37:42 -0700

    The function creates a class square with privite atrribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+    def __init__(self, size):
+        self.__size = size
