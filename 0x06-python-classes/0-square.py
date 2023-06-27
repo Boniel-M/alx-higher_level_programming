f6699d0239506397b02695f93795793256c9e739
Boniel-M <bonielwanjohi@gmail.com>
2023-06-27 04:48:34 -0700

    Added a doc string

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square that represents a square shape.
 
+"""
 class Square:
     """ A class that defines a square.
     The class does not currently have any attributes.
