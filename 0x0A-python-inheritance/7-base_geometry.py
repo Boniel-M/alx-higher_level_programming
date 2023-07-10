4aafba0322c7f13f9f1d57199bb6fd1eeade6166
Boniel-M <bonielwanjohi@gmail.com>
2023-07-10 05:32:29 -0700

    a class BaseGeometry (based on 6-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 """
 class BaseGeometry (based on 5-base_geometry.py).
 """
+
 
 
 class BaseGeometry:
@@ -10,7 +10,6 @@ class BaseGeometry:
     Base class for geometry.
 
     This class provides a base for implementing geometry-related classes.
-
     """
 
     def area(self):
