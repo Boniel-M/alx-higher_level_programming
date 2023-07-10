bb530ba7c7aaf522d0c57b33d17ab3c2258ccbb5
Boniel-M <bonielwanjohi@gmail.com>
2023-07-10 05:16:36 -0700

    a class BaseGeometry with two public instance methods

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry.
+
+    This class provides a base for implementing geometry-related classes.
+
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented.
+
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate the value as an integer.
+
+        Args:
+            name (str): The name of the value.
+            value: The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
