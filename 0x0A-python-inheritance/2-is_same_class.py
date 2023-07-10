4111c093a8107a82b8e59913822cc274012dc828
Boniel-M <bonielwanjohi@gmail.com>
2023-07-10 04:17:21 -0700

    Fixed some errors on formatting pycodestyle

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,6 @@ Check if obj is instance of a_class
 """
 
 
-
 def is_same_class(obj, a_class):
     """
     Checks if an object is exactly an instance of the specified class.
@@ -14,7 +13,8 @@ def is_same_class(obj, a_class):
         a_class: The class to compare against.
 
     Returns:
-        True if the object is exactly an instance of the specified class; otherwise False.
+        True if the object is exactly an instance of the specified class;
+        otherwise False.
 
     """
     return obj.__class__ is a_class
