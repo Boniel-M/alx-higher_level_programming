6d79ad5939c1dbf9a7147cf5e8ba7ac0d7ed04fc
Boniel-M <bonielwanjohi@gmail.com>
2023-07-10 04:12:25 -0700

    a function that returns True if the object is exactly an instance of the specified class ; otherwise False

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-"""
-a class MyList that inherits from list
-"""
-
 
 class MyList(list):
     """
@@ -17,7 +13,17 @@ class MyList(list):
     def print_sorted(self):
         """
         Prints the list in ascending order.
-        """
 
+        Example:
+            >>> my_list = MyList()
+            >>> my_list.append(1)
+            >>> my_list.append(4)
+            >>> my_list.append(2)
+            >>> my_list.append(3)
+            >>> my_list.append(5)
+            >>> my_list.print_sorted()
+            [1, 2, 3, 4, 5]
+
+        """
         sorted_list = sorted(self)
         print(sorted_list)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 
+
 def is_same_class(obj, a_class):
     """
     Checks if an object is exactly an instance of the specified class.
@@ -10,8 +11,7 @@ def is_same_class(obj, a_class):
         a_class: The class to compare against.
 
     Returns:
-        True if the object is exactly an instance of the specified class;
-        otherwise False.
+        True if the object is exactly an instance of the specified class; otherwise False.
 
     """
-    return type(obj) is a_class
+    return obj.__class__ is a_class
