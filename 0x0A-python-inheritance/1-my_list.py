f255c30d71a99615eb059e0f965a06b6b2f39c39
Boniel-M <bonielwanjohi@gmail.com>
2023-07-10 06:47:06 -0700

    a class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,29 +3,14 @@
 a class MyList that inherits from list
 """
 
-
 class MyList(list):
     """
-    A custom list class that inherits from the built-in list class.
-
-    Public Methods:
-        print_sorted: Prints the list in ascending order.
-
+    Custom list class that inherits from the built-in list class.
     """
 
     def print_sorted(self):
         """
-        Prints the list in ascending order.
-
-        Example:
-            >>> my_list = MyList()
-            >>> my_list.append(1)
-            >>> my_list.append(4)
-            >>> my_list.append(2)
-            >>> my_list.append(3)
-            >>> my_list.append(5)
-            >>> my_list.print_sorted()
-            [1, 2, 3, 4, 5]
+        Prints the list in sorted order (ascending sort).
 
         """
         sorted_list = sorted(self)
