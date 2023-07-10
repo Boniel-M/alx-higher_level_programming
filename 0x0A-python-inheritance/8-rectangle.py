b337ca434678b300d5e4d14bf14b20e28cbc7865
Boniel-M <bonielwanjohi@gmail.com>
2023-07-10 06:01:58 -0700

    a class Rectangle that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Rectangle Module
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+
+    @staticmethod
+    def area():
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    @staticmethod
+    def integer_validator(name, value):
+        """
+        Validates the value if it's an integer and greater than 0.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle instance.
+
+        Returns:
+            str: A string representation of the Rectangle instance.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
