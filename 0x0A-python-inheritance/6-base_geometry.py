5c865be79885c423164afe5f795bf6adeee0cf5e
Boniel-M <bonielwanjohi@gmail.com>
2023-07-10 05:02:03 -0700

    a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry.
+
+    This class provides a base for implementing geometry-related classes.
+
+    """
+    def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented.
+
+        """
+        raise Exception("area() is not implemented")
