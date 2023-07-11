ee39f45268b7740473029953d833c33768cff878
Boniel-M <bonielwanjohi@gmail.com>
2023-07-11 11:42:08 +0000

    a script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,6 +8,36 @@ import sys
 from collections import defaultdict
 
 
+def print_statistics(file_size, status_codes):
+    """
+    Print the computed statistics.
+
+    Args:
+        file_size (int): Total file size.
+        status_codes (dict): Number of lines by status code.
+    """
+    print("File size:", file_size)
+    for status_code in sorted(status_codes.keys()):
+        count = status_codes[status_code]
+        print(f"{status_code}: {count}")
+
+
+def parse_line(line):
+    """
+    Parse a line and extract file size and status code.
+
+    Args:
+        line (str): The line to parse.
+
+    Returns:
+        tuple: The file size and status code.
+    """
+    parts = line.split()
+    file_size = int(parts[-1])
+    status_code = int(parts[-2])
+    return file_size, status_code
+
+
 def compute_metrics():
     """
     Read input from stdin, compute metrics, and print statistics.
@@ -20,9 +50,8 @@ def compute_metrics():
         for line in sys.stdin:
             line = line.strip()
             if line:
-                parts = line.split()
-                file_size += int(parts[-1])
-                status_code = int(parts[-2])
+                file_size_delta, status_code = parse_line(line)
+                file_size += file_size_delta
                 status_codes[status_code] += 1
                 line_count += 1
 
@@ -31,20 +60,6 @@ def compute_metrics():
 
     except KeyboardInterrupt:
         print_statistics(file_size, status_codes)
-
-
-def print_statistics(file_size, status_codes):
-    """
-    Print the computed statistics.
-
-    Args:
-        file_size (int): Total file size.
-        status_codes (dict): Number of lines by status code.
-    """
-    print(f"File size: {file_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
 
 
 if __name__ == "__main__":
