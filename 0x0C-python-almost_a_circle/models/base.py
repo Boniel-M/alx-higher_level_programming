599537ecf8837fc45a8d0b692fe95f341f9c8e96
Boniel-M <bonielwanjohi@gmail.com>
2023-07-17 03:29:53 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-"""
-This module contains the base class for all other classes in the project.
-"""
+
+import json
 
 
 class Base:
@@ -27,3 +26,72 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Converts a list of dictionaries to a JSON string representation.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+
+        Returns:
+            str: JSON string representation of the list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Converts a JSON string representation to a list of dictionaries.
+
+        Args:
+            json_string (str): JSON string representation.
+
+        Returns:
+            list: List of dictionaries represented by the JSON string.
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Creates an instance with attributes set from a dictionary.
+
+        Args:
+            **dictionary: Double pointer to a dictionary.
+
+        Returns:
+            Base: An instance of the class with attributes set from the dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns a list of instances from a file.
+
+        Returns:
+            list: List of instances.
+        """
+        file_name = cls.__name__ + ".json"
+        try:
+            with open(file_name, "r") as file:
+                json_string = file.read()
+                dictionaries = cls.from_json_string(json_string)
+                instances = [cls.create(**dictionary) for dictionary in dictionaries]
+                return instances
+        except FileNotFoundError:
+            return []
