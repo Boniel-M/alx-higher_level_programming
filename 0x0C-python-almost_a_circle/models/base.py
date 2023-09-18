dda6bccdfa984bada1e88791860f9d33f96f1b3c
Boniel-M <bonielwanjohi@gmail.com>
2023-09-18 09:58:36 -0700

    Rectangle instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
+"""Base Module"""
 
-"""Base class module"""
+import json  # Import the json module
 
+
+import json
 
 class Base:
-    """Base class with a private class attribute"""
+    """
+    Base class represents the base of all other classes in the project.
+    It manages the 'id' attribute in all future classes.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initializes an instance of the Base class."""
         if id is not None:
-            """If an id is provided, assign it to the
-            public instance attribute 'id' """
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of the list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
